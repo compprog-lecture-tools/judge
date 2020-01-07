ea93eedd13db273246a6b54761f596caa929c1e7
David Stangl <david.stangl@student.hpi.de>
2020-01-07 11:29:19 +0100

    Merge migrations

--- domjudge/webapp/src/Migrations/Version20191122175149.php
+++ domjudge/webapp/src/Migrations/Version20191122175149.php
@@ -14,22 +14,24 @@ final class Version20191122175149 extends AbstractMigration
 {
     public function getDescription() : string
     {
-        return '';
+        return 'Runtime based contests';
     }
 
     public function up(Schema $schema) : void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE scorecache ADD runtime_restricted DOUBLE PRECISION NOT NULL, ADD runtime_public DOUBLE PRECISION NOT NULL');
+        $this->addSql('ALTER TABLE contest ADD order_by_runtime TINYINT(1) NOT NULL');
+        $this->addSql('ALTER TABLE rankcache ADD totalruntime_restricted DOUBLE PRECISION NOT NULL, ADD totalruntime_public DOUBLE PRECISION NOT NULL');
     }
 
     public function down(Schema $schema) : void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE scorecache DROP runtime_restricted, DROP runtime_public');
+        $this->addSql('ALTER TABLE contest DROP order_by_runtime');
+        $this->addSql('ALTER TABLE rankcache DROP totalruntime_restricted, DROP totalruntime_public');
     }
 }
